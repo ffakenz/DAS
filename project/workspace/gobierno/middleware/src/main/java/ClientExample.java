2145bb2b38bd802464e9ba818d4dfe8f9ba5bf8b
ffakenz <franco.testagrossa@gmail.com>
2018-04-09 01:47:29 -0300

    20: create RestClient

--- project/workspace/gobierno/middleware/src/main/java/ClientExample.java
+++ project/workspace/gobierno/middleware/src/main/java/ClientExample.java
@@ -4,6 +4,7 @@ import beans.PlanBean;
 
 import clients.CXFClient;
 import clients.ConcesionariaServiceContract;
+import clients.RestClient;
 import com.google.gson.*;
 
 import java.util.Arrays;
@@ -29,31 +30,42 @@ public class ClientExample {
 
         System.out.println("consultarPlanes");
         String planesJSON = client.consultarPlanes(); // use gson
-        System.out.println("planes" + planesJSON);
+        System.out.println("planes " + planesJSON);
         PlanBean[] planBeans = gson.fromJson(planesJSON, PlanBean[].class);
         Arrays.asList(planBeans).forEach(plan -> logPlan.accept(plan));
 
         System.out.println("cancelarPlan");
-        client.cancelarPlan(7L);
+        client.cancelarPlan(3L);
 
-        System.out.println("consultarPlane");
-        String planJSON = client.consultarPlan(7L); // use gson
-        System.out.println("plan" + planJSON);
+        System.out.println("consultarPlan");
+        String planJSON = client.consultarPlan(3L); // use gson
+        System.out.println("plan " + planJSON);
         PlanBean plan = gson.fromJson(planJSON, PlanBean.class);
         logPlan.accept(plan);
     };
 
-
     public static void main(String[] args) {
+        // TODO: quitar/services
         AxisClient axis =
             new AxisClient(
                 "http://192.168.1.6:8001/concesionaria_axis_one/services/ConcesionariaAxisOne.ConcesionariaAxisOneHttpEndpoint/"
                 , "http://ws.ConcesionariaAxisOne/"
             );
 
+        // TODO: quitar/services
         CXFClient cxf =
                 new CXFClient("http://192.168.1.6:8000/concesionaria_cxf_one/services/concesionaria_cxf_one?wsdl");
 
-        logClient.accept(cxf);
+        // TODO: change concesionarias_rest_one to concesionaria_rest_one
+        RestClient rest =
+                new RestClient("http://192.168.1.6:8002/concesionarias_rest_one/concesionariaRestOne");
+
+
+        // System.out.println("AXIS");
+        // logClient.accept(axis);
+        // System.out.println("CXF");
+        // logClient.accept(cxf);
+        System.out.println("REST");
+        logClient.accept(rest);
     }
 }
---
+++ project/workspace/gobierno/middleware/src/main/java/clients/RestClient.java
@@ -0,0 +1,79 @@
+package clients;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import java.io.IOException;
+import java.net.URI;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+
+public class RestClient implements ConcesionariaServiceContract {
+
+    private final String url;
+    private final HttpClient client;
+
+    public RestClient(String url) {
+        this.url = url;
+        this.client = HttpClientBuilder.create().build();
+    }
+
+    private String getUrl() { return this.url; }
+    private HttpClient getCliente() { return this.client; }
+
+    private BiFunction<String, String, HttpUriRequest> HTTPFactory = (method, callTo) -> {
+        URI uri = URI.create(getUrl() + callTo);
+        switch (method) {
+            case "POST":
+                HttpPost postReq = new HttpPost();
+                postReq.setURI(uri);
+                return postReq;
+            case "GET":
+                HttpGet getReq = new HttpGet();
+                getReq.setURI(uri);
+                return getReq;
+            case "PUT":
+                HttpPut putReq = new HttpPut();
+                putReq.setURI(uri);
+                return putReq;
+            default: throw new IllegalArgumentException("Invalid method: " + method);
+        }
+    };
+
+    private BiFunction<String, String, String> call = (method, callTo) -> {
+        try {
+            HttpResponse resp = getCliente().execute(HTTPFactory.apply(method, callTo));
+            HttpEntity responseEntity = resp.getEntity();
+            return EntityUtils.toString(responseEntity);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "No respuesta";
+    };
+
+    private BiConsumer<String, String> fireAndForget = (method, callTo) -> {
+        try {
+            getCliente().execute(HTTPFactory.apply(method, callTo));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    };
+
+    @Override
+    public String consultarPlanes() {
+        return call.apply("GET", "/consultarPlanes");
+    }
+
+    @Override
+    public String consultarPlan(Long planId) {
+        return call.apply("GET", "/consultarPlan?" + planId.toString());
+    }
+
+    @Override
+    public void cancelarPlan(Long planId) {
+        fireAndForget.accept("PUT", "/cancelarPlan?" + planId.toString());
+    }
+}
