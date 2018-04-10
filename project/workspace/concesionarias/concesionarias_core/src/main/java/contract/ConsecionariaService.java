627185b4037d9628d8f678ae67ff94328cf3dcbd
ffakenz <franco.testagrossa@gmail.com>
2018-04-10 00:05:27 -0300

    21: change dependency from DatasourceConfig

--- project/workspace/concesionarias/concesionarias_core/src/main/java/contract/ConsecionariaService.java
+++ project/workspace/concesionarias/concesionarias_core/src/main/java/contract/ConsecionariaService.java
@@ -9,11 +9,13 @@ public abstract class ConsecionariaService {
     // create mssql factory
 	protected DAOAbstractFactory abstractFactory;
 
+	abstract protected DatasourceEnum getDatasourceEnum();
+
 	// get dao for plans
 	protected PlanDAO planDAO;
 	
-    public ConsecionariaService(DAOFactory type, DatasourceEnum datasourceEnum) {
-        abstractFactory = DAOAbstractFactory.getDAOFactory(type, datasourceEnum);
+    public ConsecionariaService(DAOFactory type) {
+        abstractFactory = DAOAbstractFactory.getDAOFactory(type, getDatasourceEnum());
         planDAO = abstractFactory.getPlanDAO();
     }     
 }
--- project/workspace/concesionarias/concesionarias_core/src/main/java/contract/implementors/MSSQLConsecionaria.java
+++ project/workspace/concesionarias/concesionarias_core/src/main/java/contract/implementors/MSSQLConsecionaria.java
@@ -5,7 +5,13 @@ import dbaccess.DAOFactory;
 import dbaccess.config.DatasourceEnum;
 
 public abstract class MSSQLConsecionaria extends ConsecionariaService {
-    public MSSQLConsecionaria(DatasourceEnum datasourceEnum) {
-        super(DAOFactory.MSSQL, datasourceEnum);
-    }    
+
+    public MSSQLConsecionaria() {
+        super(DAOFactory.MSSQL);
+    }
+
+    @Override
+    protected DatasourceEnum getDatasourceEnum() {
+        return DatasourceEnum.DEFAULT;
+    }
 }
