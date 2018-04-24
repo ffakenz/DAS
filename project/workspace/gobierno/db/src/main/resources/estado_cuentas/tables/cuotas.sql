2b3f5ddbdddc589e4361d70db8826cbc409fb988
ffakenz <franco.testagrossa@gmail.com>
2018-04-23 21:06:53 -0300

    27: add estado_cuenta db first draft

---
+++ project/workspace/gobierno/db/src/main/resources/estado_cuentas/tables/cuotas.sql
@@ -0,0 +1,9 @@
+CREATE TABLE cuotas (
+    estado_cuenta_id BIGINT NOT NULL
+    , id BIGINT NOT NULL
+    , fecha_vencimiento DATETIME NOT NULL
+    , monto INT NULL
+    , fecha_pago DATETIME NULL
+    , PRIMARY KEY(estado_cuenta_id, id)
+    , FOREIGN KEY(estado_cuenta_id) REFERENCES estado_cuentas(id)
+);
