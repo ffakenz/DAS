5dfc6fca317d35e01b20291347718d3ec3484ebf
ffakenz <franco.testagrossa@gmail.com>
2018-04-24 16:16:17 -0300

    26: patch serial for consultar Estado cuenta

--- project/workspace/gobierno/interactors/src/main/java/estado_cuentas/EstadoCuentaInteractor.java
+++ project/workspace/gobierno/interactors/src/main/java/estado_cuentas/EstadoCuentaInteractor.java
@@ -2,16 +2,20 @@ package estado_cuentas;
 
 import ar.edu.ubp.das.mvc.db.Dao;
 import concesionarias.boundaries.Utils;
+import estado_cuentas.boundaries.ConsultarEstadoCuentas;
 import estado_cuentas.boundaries.RegistrarCuota;
 import estado_cuentas.boundaries.RegistrarEstadoCuenta;
 import estado_cuentas.forms.CuotaForm;
 import estado_cuentas.forms.EstadoCuentaForm;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
-public class EstadoCuentaInteractor implements RegistrarEstadoCuenta, RegistrarCuota,Utils {
+public class EstadoCuentaInteractor implements RegistrarEstadoCuenta, RegistrarCuota, ConsultarEstadoCuentas, Utils {
 
     @Override
     public Function<Dao, Optional<Long>> registrarEstadoCuenta(EstadoCuentaForm form) {
@@ -46,6 +50,21 @@ public class EstadoCuentaInteractor implements RegistrarEstadoCuenta, RegistrarC
                 c.setItem("id", c.getId().toString());
                 return c.getEstadoCuentaId() == form.getEstadoCuentaId();
             }).apply(dao);
+        };
+    }
+
+    @Override
+    public Function<Dao, List<EstadoCuentaForm>> consultarEstadoCuentas() {
+        return dao -> {
+            try {
+                return
+                    dao.select(null).stream()
+                        .map(d -> (EstadoCuentaForm) d)
+                        .collect(Collectors.toList());
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                return new ArrayList<>();
+            }
         };
     }
 }
