5d2bfa905c74fe38b396b6f6e19ffbc409cc88b9
Franco Testagrossa <franco@tiendanube.com>
2018-08-16 22:10:08 -0300

    Chau querido amigo
    Muchas gracias por todo! te vamos a extrañar!
    gracias por tanto perdon por tan poco! <3

--- project/workspace/gobierno/db/src/main/resources/startup/mssql_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Das12345' -p 1433:1433 --name mssql -d microsoft/mssql-server-linux:2017-latest
-# docker exec -it mssql "bash"
-# /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Das12345
-# CREATE DATABASE db_gobierno;
-# GO
