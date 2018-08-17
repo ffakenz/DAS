10d35aea9aae532716467a6733bd0cbeac2a731d
iroccalada <ignacio.rocca@mercadolibre.com>
2018-08-16 21:34:42 -0300

    add docker magic

---
+++ project/workspace/gobierno/db/src/main/resources/run_docker.sh
@@ -0,0 +1,1 @@
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Das12345' -p 1433:1433 --name mssql -d microsoft/mssql-server-linux:2017-latest && docker exec -ti mssql sh -c "/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Das12345 -Q \"CREATE DATABASE db_gobierno;\""
