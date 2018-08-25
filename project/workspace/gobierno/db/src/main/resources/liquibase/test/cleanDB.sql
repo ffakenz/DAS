4547ccd34caac1732473a438e3aeb6278d7b0f0c
Franco Testagrossa <franco@tiendanube.com>
2018-08-25 18:06:53 -0300

    Remove transaction from cleanDB procedure

--- project/workspace/gobierno/db/src/main/resources/liquibase/test/cleanDB.sql
+++ project/workspace/gobierno/db/src/main/resources/liquibase/test/cleanDB.sql
@@ -1,23 +1,15 @@
 CREATE PROCEDURE cleanDB AS
-BEGIN TRY
-    BEGIN TRANSACTION
-       -- disable all constraints
-       EXEC sp_msforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"
+BEGIN
+   -- disable all constraints
+   EXEC sp_msforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"
 
-       -- delete data in all tables
-       EXEC sp_MSForEachTable "DELETE FROM ?"
+   -- delete data in all tables
+   EXEC sp_MSForEachTable "DELETE FROM ?"
 
-       -- enable all constraints
-       EXEC sp_msforeachtable "ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all"
+   -- enable all constraints
+   EXEC sp_msforeachtable "ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all"
 
-       --some of the tables have identity columns we may want to reseed them
-       EXEC sp_MSforeachtable "DBCC CHECKIDENT ( '?', RESEED, 0)"
-   COMMIT TRANSACTION
-END TRY
-BEGIN CATCH
-    IF (@@TRANCOUNT > 0)
-      BEGIN
-          ROLLBACK TRANSACTION
-      END
-END CATCH
+   --some of the tables have identity columns we may want to reseed them
+   EXEC sp_MSforeachtable "DBCC CHECKIDENT ( '?', RESEED, 0)"
+END
 GO
