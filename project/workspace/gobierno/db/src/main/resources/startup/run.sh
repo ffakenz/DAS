6248809e94d6ec65918817a207c5ba9b9eb8aaf4
Franco Testagrossa <franco@tiendanube.com>
2018-09-02 20:15:17 -0300

    adding echo to run  script

--- project/workspace/gobierno/db/src/main/resources/startup/run.sh
+++ project/workspace/gobierno/db/src/main/resources/startup/run.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+echo "running docker container"
 docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Das12345' -p 1433:1433 --name mssql -d microsoft/mssql-server-linux:2017-latest
+echo "creating db_gobierno"
 docker exec -d mssql sh -c "/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Das12345 -Q \"CREATE DATABASE db_gobierno;\""
+echo "db_gobierno created"
