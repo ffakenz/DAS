6ab504f72d7f05c2996a84ca8b627aef63de7b31
Franco Testagrossa <franco@tiendanube.com>
2018-12-01 03:09:11 -0300

    Adding build, start, stop and test db scripts to gobierno

--- project/workspace/gobierno/db/src/main/resources/startup/run.sh
+++ project/workspace/gobierno/db/src/main/resources/startup/run.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
-echo "running docker container"
-docker run -p 1433:1433 --name mssql_gobierno -d mssql_gobierno:1.0
-echo "creating db_gobierno" &&
-docker exec -d mssql_gobierno sh -c "./sqlcmd -S localhost -U SA -P Das12345 -i ./scripts/script.sql"
-echo "db_gobierno created"
+
+function run_docker() {
+    PORT=$1
+    echo "Running mssql_gobierno"
+    docker run -p 1433:1433 --name mssql_gobierno -d mssql_gobierno:1.0
+}
+function db_install() {
+    PORT=$1
+    echo "Installing DB in mssql_gobierno"
+    docker exec -d mssql_gobierno sh -c "./sqlcmd -S localhost -U SA -P Das12345 -i ./scripts/script.sql"
+}
+
+echo "Running Docker container"
+run_docker 1433
+echo "Awaiting"
+sleep 15
+echo "Installing DB"
+db_install 1433
---
+++ project/workspace/gobierno/db/src/main/resources/startup/test_docker_db.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+function test_db(){
+    echo "Connecting to container mssql_gobierno"
+    docker exec -ti mssql_gobierno /bin/bash -c "./sqlcmd -S localhost -U SA -P Das12345"
+}
+
+# Example of usages
+# test_db 1433
+# use db_gobierno
