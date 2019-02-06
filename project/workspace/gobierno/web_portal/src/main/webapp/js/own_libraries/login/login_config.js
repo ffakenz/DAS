8bcb327984d8e3971dfc62df631c2ce0965def51
iroccalada <ignacio.rocca@mercadolibre.com>
2019-02-06 17:55:21 -0300

    fix login_config

--- project/workspace/gobierno/web_portal/src/main/webapp/js/own_libraries/login/login_config.js
+++ project/workspace/gobierno/web_portal/src/main/webapp/js/own_libraries/login/login_config.js
@@ -11,7 +11,6 @@ const LoginConfig = (module) => [
         ctx: Class.LOGINBOX,
         cnfg: [
             { delegate: Id.FIRST_LOGIN_BTN, handler: module.goToPrimerIngreso },
-            { delegate: Id.LOGIN_REGISTRAR_BTN, handler: module.registrarUsuario },
             { delegate: Id.LOGIN_BTN, handler: module.validarUsuario }
         ]
     }
